ea8a75c37fcea0bc68250a202d75d6c881de5633
Leo <leo.player@education.nsw.gov.au>
2019-02-08 23:10:20 +1100

    Hello World score system
    YAY
    v0.2?α

--- HelloWorld/src/sample/SampleController.java
+++ HelloWorld/src/sample/SampleController.java
@@ -5,12 +5,22 @@ import javafx.scene.control.Label;
 
 public class SampleController {
     public Label helloWorld;
+    public Label score;
+    private int scoreMark;
+    private int score2 = scoreMark + 1;
 
     public void sayHelloWorld(ActionEvent actionEvent) {
         helloWorld.setText("Hello World!");
+        scoreMark = 0;
     }
 
     public void dontSayHelloWorld(ActionEvent actionEvent) {
-        helloWorld.setText("");
+        if (scoreMark == 0){
+            helloWorld.setText("");
+            scoreMark = 1;
+            score.setText(String.valueOf(score2));
+            scoreMark = score2;
+            score2 = score2 + 1;
+        }
     }
 }
