261616186e29e7f193c93cad004d571a0a6068e2
Leo <leo.player@education.nsw.gov.au>
2019-02-21 17:25:30 +1100

    Stuff1 and Spam

--- Testing/indexFiles/index.js
+++ Testing/indexFiles/index.js
@@ -1,6 +1,9 @@
 function timedAlert() {
-    document.getElementById("index").innerHTML = alert("wee");
+    while (true) {
+        alert("wee");
+    }
 }
+
 function date() {
     document.getElementById('dateText').innerHTML = Date();
 }
