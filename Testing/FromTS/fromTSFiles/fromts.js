8d2ac8c8e2591c08a25f46303beb60dff54f74eb
Leo <leo.player@education.nsw.gov.au>
2019-03-25 07:08:59 +1100

    Good Testing
    final testing only upload

--- Testing/FromTS/fromTSFiles/fromts.js
+++ Testing/FromTS/fromTSFiles/fromts.js
@@ -2,15 +2,3 @@ function openInNewTab(url) {
     var win = window.open(url, '_blank');
     win.focus();
 }
-
-function play {
-
-  var video=document.getElementById("autoplay");
-
-  if(video.muted){
-    video.muted = false;
-  } else {
-    video.muted = true;
-  }
-
-}
